5dbcfc423b677b59a332c7fd44d35d78a359d338
Maël Mouquet <mael.mqt@gmail.com>
2024-02-13 11:30:48 +0100

    images

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify
+from flask_uploads import UploadSet, configure_uploads, IMAGES
 import numpy as np
 import pandas as pd 
 import pickle
@@ -6,6 +7,28 @@ import pickle
 app = Flask(__name__)
 model = pickle.load(open('model.pickle', 'rb'))
 cols = ['MODELYEAR', 'ENGINESIZE', 'CYLINDERS', 'FUELCONSUMPTION_COMB']
+
+from keras.applications.resnet50 import ResNet50
+rnModel = ResNet50()
+from keras.applications.resnet50 import preprocess_input
+from keras.preprocessing.image import load_img, img_to_array
+from keras.applications.resnet50 import decode_predictions
+
+def predictImageCategory(imagePath):
+    # voir TP1 pour VGG16
+    image = load_img(imagePath, target_size=(224, 224))
+    image = img_to_array(image)
+    image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+    image = preprocess_input(image)
+
+    prediction = rnModel.predict(image)
+    label = decode_predictions(prediction)
+    label = label[0][0]
+    return (label[1], label[2]*100, "%")
+
+photos = UploadSet('photos', IMAGES)
+app.config['UPLOADED_PHOTOS_DEST'] = './static/img'
+configure_uploads(app, photos)
 
 @app.route('/')
 def home():
@@ -28,6 +51,14 @@ def predict_api():
     prediction=model.predict(data_unseen)
     result=prediction[0]
     return jsonify(result)
+
+@app.route('/upload', methods=['POST', 'GET'])
+def upload():
+    if 'photos' in request.files:
+        filename = photos.save(request.files['photos'])
+        prediction = predictImageCategory(f'./static/img/{filename}')
+        return render_template('upload.html', filename=filename, prediction=prediction)
+    return render_template('upload.html')
 
 if __name__ == '__main__':
     app.run()
